56cbfa4da08fc3c3c801ce2a3e3f8e43c1490c1e
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-01-21 15:22:34 +0000

    change title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
                 <Container px={2} maxWidth="container.md">
                     <Flex alignItems="center">
                         <Heading color={primaryTextColor} size="sm">
-                            <Link to="/">Single Board</Link>
+                            <Link to="/">Single Board PR-TEST</Link>
                         </Heading>
                         <Spacer />
                         {user ? (
