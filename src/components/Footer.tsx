7d160b6c4475d0fc00de475176401b691b1d7a9f
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-01-25 05:04:43 +0000

    フッターに各情報を記載

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,15 @@
+import {VStack, Text,Link } from "@chakra-ui/react";
+import { ExternalLinkIcon} from "@chakra-ui/icons";
+
 export const Footer = () => {
-    return <div />;
+    return (<>
+        <VStack justify='center' mt={5} mb={5} color="gray.700" fontSize="sm">
+            <Text mt={0} mb={0}>
+                このページはReact.jsで構築され、Firebase Hostingによってホストされています
+            </Text>
+            <Link href='https://github.com/skuralll/single-board' isExternal>
+                GitHub Repository<ExternalLinkIcon mx='2px' />
+            </Link>
+        </VStack>
+    </>);
 };
